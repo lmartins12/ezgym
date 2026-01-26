bae54bd02064806b1f4467dc6a3d5730a4692680
Lucas Martins <lucas.martins559@outlook.com>
2026-01-26 16:14:49 -0300

    refactor(quick-create-workout): remove modal depreciado

--- src/app/features/workouts/components/index.ts
+++ src/app/features/workouts/components/index.ts
@@ -1,5 +1,4 @@
-export * from './workout-card/workout-card.component';
-export * from './exercise-list-item/exercise-list-item.component';
-export * from './quick-create-workout-modal/quick-create-workout-modal.component';
 export * from './edit-workout-modal/edit-workout-modal.component';
 export * from './exercise-editor-modal/exercise-editor-modal.component';
+export * from './exercise-list-item/exercise-list-item.component';
+export * from './workout-card/workout-card.component';
